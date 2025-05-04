bc462e787e7a0a9dbd6804655aa65a60cb96243b
dolevoper <omerdolev90@gmail.com>
2025-05-04 19:34:20 +0300

    add buttons

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,23 +2,26 @@ import { useState } from "react";
 import styles from "./Counter.module.scss";
 
 export function Counter() {
-    console.log("Render Counter");
     const [count, setCount] = useState(0);
 
     return (
         <article className={styles.container}>
-            <button>➗2</button>
+            <button onClick={() => {
+                setCount(count / 2);
+            }}>➗2</button>
             <button onClick={() => {
                 setCount(count - 1);
-                console.log(count);
             }}>➖1</button>
             <span>{count}</span>
             <button onClick={() => {
                 setCount(count + 1);
-                console.log(count);
             }}>➕1</button>
-            <button>✖️2</button>
-            <button>Reset</button>
+            <button onClick={() => {
+                setCount(count * 2);
+            }}>✖️2</button>
+            <button onClick={() => {
+                setCount(0);
+            }}>Reset</button>
         </article>
     );
 }
