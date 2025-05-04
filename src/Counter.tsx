15f3ffe9e387849f112bcf194fc140afdbb4b3c9
amit reuveni <amitr123r@gmail.com>
2025-05-04 20:43:47 +0300

    .

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,28 +1,5 @@
 import styles from "./Counter.module.scss";
 
-<<<<<<< HEAD
-export function Counter() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <article className={styles.container}>
-            <button onClick={() => {
-                setCount(count / 2);
-            }}>➗2</button>
-            <button onClick={() => {
-                setCount(count - 1);
-            }}>➖1</button>
-            <span>{count}</span>
-            <button onClick={() => {
-                setCount(count + 1);
-            }}>➕1</button>
-            <button onClick={() => {
-                setCount(count * 2)
-            }}>✖️2</button>
-            <button onClick={() => {
-                setCount(0);
-            }}>Reset</button>
-=======
 type CounterProps = {
     count: number;
     onHalfClick: () => void;
@@ -40,7 +17,6 @@ export function Counter({ count, onHalfClick, onDecreaseClick, onIncreaseClick, 
             <button onClick={onIncreaseClick}>➕1</button>
             <button onClick={onDoubleClick}>✖️2</button>
             <button onClick={onResetClick}>Reset</button>
->>>>>>> 0b5a51012e030be945ac48beef61c8e8125d1e1d
         </article>
     );
 }
