0b5a51012e030be945ac48beef61c8e8125d1e1d
dolevoper <omerdolev90@gmail.com>
2025-05-04 20:40:34 +0300

    counter list task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 import { Counter } from "./Counter";
 
+// 1. add "SPA navigation" - menu with button to show the synced counters and another button to show the counter list
+// 2. implement "static" counter list - show 4 counters with individual states
+// 3. implement "dynamic" counter list
+//      - show one button: "New counter" - clicking on it will add a counter to the screen
+//      - next to each counter show a delete button to remove the counter from the screen
+//      - counters should not be synced (each should have an individual state)
+// Bonus - maintain the state of the counters when "navigating" back and forth  between the components
+
 function App() {
+  return (
+    <>
+      <SyncedCounters />
+      <CounterList />
+    </>
+  );
+}
+
+function SyncedCounters() {
   const [count, setCount] = useState(0);
 
   function half() {
@@ -31,6 +48,12 @@ function App() {
       <Counter count={count} onHalfClick={half} onDecreaseClick={decrease} onIncreaseClick={increase} onDoubleClick={double} onResetClick={reset} />
       <Counter count={count} onHalfClick={half} onDecreaseClick={decrease} onIncreaseClick={increase} onDoubleClick={double} onResetClick={reset} />
     </>
+  );
+}
+
+function CounterList() {
+  return (
+    <div>Counter list</div>
   );
 }
 
