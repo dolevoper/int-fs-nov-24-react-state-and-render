956248b0870ca90fae313ec8467c81a6f04d6964
amit reuveni <amitr123r@gmail.com>
2025-05-06 14:43:53 +0300

    finish stage 3

--- src/components/CounterList.tsx
+++ src/components/CounterList.tsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
+import { UnSyncedCounter } from "./UnSyncedCounter";
+import styles from "./CounterList.module.scss"
 
 
 export function CounterList() {
-    const [counters, setCounters] = useState<number[]>([]);
-  
-    function newCounter() {
-      setCounters([...counters, 0]);
-    }
-    
-    return (
-      <>
-        <button onClick={newCounter}>New counter</button>
-        <ul>
-          {counters.map((count, index) => <li key={index}>{count}</li>)}
-        </ul>
-      </>
-    );
+  const [counters, setCounters] = useState<{ id: number }[]>([]);
+
+  function newCounter() {
+    setCounters([...counters, { id: Date.now() }]);
   }
+
+  function deleteCounter(id: number) {
+    setCounters(counters.filter((counter) => counter.id !== id))
+  }
+
+  function deleteAllCounters() {
+    setCounters([]);
+  }
+
+  return (
+    <>
+      <button onClick={newCounter}>New counter</button>
+      <button onClick={deleteAllCounters}>Delete All Counters</button>
+      <ul className={styles.list}>
+        {counters.map((counter) => <li key={counter.id}>
+          <UnSyncedCounter />
+          <button onClick={() => deleteCounter(counter.id)}>Delete Counter</button>
+        </li>)}
+
+      </ul>
+    </>
+  );
+}
