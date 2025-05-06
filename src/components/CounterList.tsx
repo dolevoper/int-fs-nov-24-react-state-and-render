47a28226614dafde8a869ead7369038709c3c112
amit reuveni <amitr123r@gmail.com>
2025-05-06 18:14:39 +0300

    finish stage 4

--- src/components/CounterList.tsx
+++ src/components/CounterList.tsx
@@ -18,13 +18,26 @@ export function CounterList() {
     setCounters([]);
   }
 
+  function moveCounter(fromIndex: number, toIndex: number) {
+    const updatedCounters = [...counters];
+    const [movedCounter] = updatedCounters.splice(fromIndex, 1);
+    updatedCounters.splice(toIndex, 0, movedCounter);
+    setCounters(updatedCounters); 
+  }
+
   return (
     <>
       <button onClick={newCounter}>New counter</button>
       <button onClick={deleteAllCounters}>Delete All Counters</button>
       <ul className={styles.list}>
-        {counters.map((counter) => <li key={counter.id}>
+        {counters.map((counter, index) => <li key={counter.id}>
           <UnSyncedCounter />
+          {index > 0 && (
+            <button onClick={() => {moveCounter(index, index - 1)}}>⬆️</button>
+          )}
+          {index < counters.length - 1 && (
+            <button onClick={() => { moveCounter(index, index + 1)}}>⬇️</button>
+          )}
           <button onClick={() => deleteCounter(counter.id)}>Delete Counter</button>
         </li>)}
 
